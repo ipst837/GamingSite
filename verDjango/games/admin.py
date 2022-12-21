014cbbc71b95eb20e5c8ff18b0d85d555bbcb822
ipst837 <96713892+ipst837@users.noreply.github.com>
2022-12-21 23:39:27 +0900

    Delete verDjango directory

--- verDjango/games/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import GamesList
-
-
-class ListAdmin(admin.ModelAdmin):
-    search_fields = ['name']
-
-
-admin.site.register(GamesList, ListAdmin)
--- verDjango/profile_Lee/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render, redirect
-
-
-def main_page(request):
-    return render(request, "profile/main.html")
